2451c76188ea30b40a26e4cb9d2046e93283fa67
Christopher-SNC <christopher.snc.dev@gmail.com>
2026-09-19 21:39:46 -0300

    Cria entidade Disponibilidade com mapeamento JPA

---
+++ backend/src/main/java/br/com/projetobarbearia/entity/Disponibilidade.java
@@ -0,0 +1,95 @@
+package br.com.projetobarbearia.entity;
+
+import java.time.LocalTime;
+
+import br.com.projetobarbearia.enums.DiaSemana;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "disponibilidade")
+public class Disponibilidade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_disponibilidade")
+    private Long idDisponibilidade;
+
+    @ManyToOne
+    @JoinColumn(name = "id_barbeiro", nullable = false)
+    private Barbeiro barbeiro;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "dia_semana", nullable = false, length = 15)
+    private DiaSemana diaSemana;
+
+    @Column(name = "hora_inicio", nullable = false)
+    private LocalTime horaInicio;
+
+    @Column(name = "hora_fim", nullable = false)
+    private LocalTime horaFim;
+
+    @Column(name = "ativo", nullable = false)
+    private boolean ativo = true;
+
+    public Disponibilidade() {
+    }
+
+    public Long getIdDisponibilidade() {
+        return idDisponibilidade;
+    }
+
+    public void setIdDisponibilidade(Long idDisponibilidade) {
+        this.idDisponibilidade = idDisponibilidade;
+    }
+
+    public Barbeiro getBarbeiro() {
+        return barbeiro;
+    }
+
+    public void setBarbeiro(Barbeiro barbeiro) {
+        this.barbeiro = barbeiro;
+    }
+
+    public DiaSemana getDiaSemana() {
+        return diaSemana;
+    }
+
+    public void setDiaSemana(DiaSemana diaSemana) {
+        this.diaSemana = diaSemana;
+    }
+
+    public LocalTime getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(LocalTime horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public LocalTime getHoraFim() {
+        return horaFim;
+    }
+
+    public void setHoraFim(LocalTime horaFim) {
+        this.horaFim = horaFim;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    
+}
