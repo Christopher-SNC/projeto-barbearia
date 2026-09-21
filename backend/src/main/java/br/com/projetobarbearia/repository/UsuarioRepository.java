365db2509273d36a4a15ef3927eb907782555cfd
Christopher-SNC <christopher.snc.dev@gmail.com>
2026-09-21 10:25:09 -0300

    Cria UsuarioService com regras iniciais de usuario

--- backend/src/main/java/br/com/projetobarbearia/repository/UsuarioRepository.java
+++ backend/src/main/java/br/com/projetobarbearia/repository/UsuarioRepository.java
@@ -1,9 +1,13 @@
 package br.com.projetobarbearia.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.projetobarbearia.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
 
 }
