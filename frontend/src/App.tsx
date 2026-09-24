b866e13f9bed4635704fdc214e61d7a8ebe8d605
Christopher-SNC <christopher.snc.dev@gmail.com>
2026-09-24 18:53:09 -0300

    Cria estrutura inicial do frontend React

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="home">
+      <section className="hero">
+        <p className="subtitle">Projeto Barbearia</p>
+
+        <h1>Encontre sua barbearia e agende seu horário.</h1>
+
+        <p className="description">
+          Encontre barbearias, consulte serviços, escolha seu barbeiro
+          e marque seu atendimento.
+        </p>
+      </section>
+    </main>
+  )
+}
+
+export default App
